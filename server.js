5a26fac1d03ed1304e68937c3ed39994fbd55b4a
HyrGlobalSystem <sasidhareee.reddy@gmail.comn>
2018-10-13 22:13:13 -0500

    Intial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+const app=express();
+
+hbs.registerPartials(__dirname+"/views/partials");
+app.set('view engine','hbs');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var logger=`${now}:${req.method} ${req.url}`
+    fs.appendFile('server.log',logger+"\n",(err)=>{
+        console.log('Unable to append a file');
+    });
+    console.log(logger);
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render("maintance.hbs");
+// });
+
+app.use(express.static(__dirname+"/public"));
+
+app.get('/', (req,res)=>{
+    //res.send('<h1>Hello Express</h1>');
+   res.render("home.hbs",{
+       pageTitle:"HomePage",
+       message:"Welcome this is hbs node application"
+   });
+})
+app.get('/about',(req,res)=>{
+    res.render("about.hbs",{
+        pageTitle:"AboutPage",
+    });
+})
+
+
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        error:"Unable to fetch a page"
+    });
+})
+
+app.listen(3000,()=>{
+    console.log("server is up running at port 3000");
+});
